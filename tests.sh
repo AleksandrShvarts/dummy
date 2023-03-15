31345176a7dcfeeb58d08a0559201194e11158be
aleksandr.shvarts@qunatory.com <Aleksandr Shvarts>
2023-03-15 19:00:25 +0400

    Test 0 if action does not runs with push not in dev

---
+++ tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+git checkout dev
+git pull
+git checkout -b "test_1_$1"
+touch "test_1_$1.txt"
+git add .
+git commit -m "Test $1 if action does not runs with push not in dev"
+git push --set-upstream origin "test_1_$1"
+
+git checkout dev
+git pull
+touch "test_2_$1.txt"
+git add .
+git commit -m "Test $1 if action runs with push in dev"
+git push --set-upstream origin "test_2_$1"
+

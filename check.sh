6014ab9cd6b81c82f3097f8d94e83c52eb47222b
aleksandr.shvarts@qunatory.com <Aleksandr Shvarts>
2023-03-16 00:48:53 +0400

    Test mnb 1 if action does not runs with push not to dev

--- check.sh
+++ check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if $(git merge-base --is-ancestor $1 HEAD);
+if [[ 0==$(git merge-base --is-ancestor $1 HEAD 2>/dev/null) ]];
 then echo "true"
 else echo "false"
-fi
+fi 
 # echo $(git merge-base --is-ancestor 110564f8eb08e259397d2f3fb807e198d7172449 HEAD)
 

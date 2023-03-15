4881db81a7c4362b66001a5753af197141a3d1d5
aleksandr.shvarts@qunatory.com <Aleksandr Shvarts>
2023-03-15 22:34:32 +0400

    Test  1 if action does not runs with push not to dev

---
+++ check.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if $(git merge-base --is-ancestor $1 HEAD);
+then echo "true"
+else echo "false"
+fi
+# echo $(git merge-base --is-ancestor 110564f8eb08e259397d2f3fb807e198d7172449 HEAD)
+
